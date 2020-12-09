23e93f346e1222727231dd5a6c743571e66e1d78
Manuel Valera <dap21c@gmail.com>
2020-12-08 19:30:55 -0800

    Working on bug on top of temperature

--- LockReleasev1.m
+++ LockReleasev1.m
@@ -23,7 +23,7 @@ m = IMax-1; n = JMax-1;
 xlength = linspace(-16,16,n+1);
 ylength = linspace(-1,1,m+1);
 
-[X,Y] = meshgrid(xlength,ylength);
+[X,Y] = ndgrid(xlength,ylength);
 %%
 
 uic = zeros(IMax ,JMax);
@@ -42,22 +42,22 @@ S = zeros(IMax+1,JMax+1);
 xlength = linspace(-16,16,n);
 ylength = linspace(-1,1,m);
 
-[Xc,Yc] = meshgrid(xlength,ylength);
+[Xc,Yc] = ndgrid(xlength,ylength);
 
 xlength = linspace(-16,16,n+1);
 ylength = linspace(-1,1,m);
 
-[Xu,Yu] = meshgrid(xlength,ylength);
+[Xu,Yu] = ndgrid(xlength,ylength);
 
 xlength = linspace(-16,16,n);
 ylength = linspace(-1,1,m+1);
 
-[Xv,Yv] = meshgrid(xlength,ylength);
+[Xv,Yv] = ndgrid(xlength,ylength);
 
 xlength = linspace(-16,16,n+2);
 ylength = linspace(-1,1,m+2);
 
-[Xp,Yp] = meshgrid([1 1.5 : 1 : JMax-0.5 JMax], [1 1.5 : 1 : IMax-0.5 IMax]);
+[Xp,Yp] = ndgrid([1 1.5 : 1 : JMax-0.5 JMax], [1 1.5 : 1 : IMax-0.5 IMax]);
 
 Xp = Xp - JMax/2.0;
 XMax = max(max(Xp));
@@ -80,7 +80,7 @@ T0 = TMAX*100;
 for j=1:JMax
     for i=1:IMax
 
-        Tic(i,j) = TMIN + (TMAX-TMIN)*(0.5-0.5*erf(X(i,j)/0.01));
+        Tic(i,j) = TMIN + (TMAX-TMIN)*(0.5-0.5*erf(Y(i,j)/0.01));
         
     
         end
@@ -93,7 +93,9 @@ title('T')
 colorbar;
 shading interp;
 
-Tatc = Tic/T0;
+Tic = Tic/T0;
+
+T = Tic;
 
 %%
 rho_ref = 1027.d0;
@@ -105,18 +107,33 @@ LStar = 0.05;
 UStar = 1.0;
 gforce = 9.80;
 
-dens = rho_ref + drho_dT*(Tatc*T0-T_ref);  %EOS  
+for j=1:JMax
+    for i=1:IMax
+
+        dens(i,j) = rho_ref + drho_dT*(T(i,j)*T0-T_ref);  %EOS  
+        
+    end
+end
+
 
 DMax = max(max(dens));
 DMin = min(min(dens));
 
 rho_P0 = (DMax+DMin)/2.0;
 
-buoy = dens;
+figure(6);
+u_v = squeeze(dens);
+pcolor(u_v);
+title('rho')
+colorbar;
+shading interp;
 
-buoy(1,:) = gforce*LStar*(dens(1,:)-rho_P0)/(rho_P0*(UStar^2));
+%%
+buoy = zeros(IMax,JMax-1);
 
-for j=2:JMax
+buoy(1,:) = gforce*LStar*(dens(1,1:n)-rho_P0)/(rho_P0*(UStar^2));
+
+for j=1:JMax-1
     for i=1:IMax-1
 
         buoy(i,j) = gforce*LStar*(0.5*(dens(i,j)+dens(i+1,j))-rho_P0)/(rho_P0*(UStar^2));
@@ -124,17 +141,44 @@ for j=2:JMax
     end
 end
 
+buoy(end,:) = buoy(end-1,:);
+
+figure(6);
+u_v = squeeze(buoy);
+pcolor(u_v);
+title('(rho-rho_0)/rho_0')
+colorbar;
+shading interp;
+
+bic = buoy;
+
+
+%%
+% 
+% 
+% for j=1:JMax
+%     for i=1:IMax
+% 
+%         uic(i,j) = X(i,j)/16;
+%         vic(i,j) = Y(i,j);
+%         
+%     
+%         end
+% end
+
+
 
 
 %%  Creating operator
 k = 2; % Mimetic order of accuracy %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Dy = Xc(1,2)-Xc(1,1);
+Dy = Xc(2,1)-Xc(1,1);
 Dx = Dy;
 D = div2D(k,m,Dx,n,Dy); %div2DCurv(k,X,Y);
 G =  grad2D(k,m,Dx,n,Dy); % grad2DCurv(k,X,Y);
 
-L = D*G;  %lap2D(k,IMax-1,Dx,JMax-1,Dy);
-L = L + robinBC2D(k,m,Dx,n,Dy,1,0);
+%L = D*G;  %lap2D(k,IMax-1,Dx,JMax-1,Dy);
+L = lap2D(k,IMax-1,Dx,JMax-1,Dy);
+L = L + robinBC2D(k,m,Dx,n,Dy,0,1);
 
 idx1 = numel(uic);
 idy1 = numel(vic);
@@ -142,35 +186,46 @@ idx2 = idx1 + idy1;
 
 
 dt = 0.1;
-rho = 1;
+rho = rho_P0;
 D = rho/dt*D;
 G = -dt/rho*G;
 %% Preparing t=0 arrays
+
+uic = zeros(IMax ,JMax);
+vic = zeros(IMax ,JMax);
+
 
 iatu = (X(1:IMax-1,:)+X(2:IMax,:))*0.50;
 jatu = (Y(1:IMax-1,:)+Y(2:IMax,:))*0.50;
 
 iatv = (X(:,1:JMax-1)+X(:,2:JMax))*0.50;
 jatv = (Y(:,1:JMax-1)+Y(:,2:JMax))*0.50;
-    
-uatu = interp2(X, Y, uic, iatu,jatu); %u-space
-uatv = interp2(X, Y, uic, iatv,jatv);
 
-vatv = interp2(X, Y, vic, iatv,jatv);
-patv = interp2(X, Y, pic, iatv,jatv);
-vatu = interp2(X, Y, vic, iatu,jatu);
-patu = interp2(X, Y, pic, iatu,jatu);
+Fuic = griddedInterpolant(X, Y, uic); 
+Fvic = griddedInterpolant(X, Y, vic); 
+Fpic = griddedInterpolant(X, Y, pic); 
+%FTic = griddedInterpolant(X, Y, Tatc); 
 
-uatp = interp2(X, Y, uic, Xp, Yp);
-vatp = interp2(X, Y, vic, Xp, Yp);
-patp = interp2(X, Y, pic, Xp, Yp);
 
-u = uatp;
-v = vatp;
-p = patp;
+uatu = Fuic(iatu,jatu);
+vatu = Fvic(iatu,jatu);
+patu = Fpic(iatu,jatu);
 
-   figure(2);
-    u_v = squeeze(uatv(:,:));
+uatv = Fuic(iatv,jatv);
+vatv = Fvic(iatv,jatv);
+patv = Fpic(iatv,jatv);
+
+u = Fuic(Xp,Yp);
+v = Fvic(Xp,Yp);
+p = Fpic(Xp,Yp);
+%T = Fpic(Xp,Yp);
+
+T = Tic;
+buoy = bic;
+
+% 
+   figure(1);
+    u_v = squeeze(uatu(:,:));
     subplot(2,2,1)
     pcolor(u_v);
     title('u')
@@ -183,26 +238,23 @@ p = patp;
     colorbar;
     shading interp;
     subplot(2,2,3)
-    u_3 = squeeze(vatu(:,:));
+    u_3 = squeeze(vatv(:,:));
     pcolor(u_3);
     title('v')
     colorbar;
     shading interp;
     subplot(2,2,4)
-    pcolor(Xp,Yp,cav)
-    hold on
-    quiver(Xp,Yp,u,v)
-    hold off
-    xlim([0 1])
-    ylim([0 1])
+    u_3 = squeeze(T(:,:));
+    pcolor(u_3);
+    title('T')
     colorbar;
     shading interp;
-    title(['vorticity Dt = ' num2str((time+1)*dt)])
-%%
+
+
 % Reshaping
 
 
-Vv = reshape(vatv',[], 1);
+Vv = reshape(vatv',[], 1);    %Do we need to transpose now with ndgrid?
 Uv = reshape(uatv',[], 1);
 Pv = reshape(patv',[], 1);
 
@@ -217,7 +269,7 @@ Pp = reshape(p',[], 1);
 cs = 1447.0;
 
 
-tf = 0.1;
+tf = 1.7;
 nf = tf/dt ;
 beta = 1/cs;
 Re = 750;
@@ -227,17 +279,17 @@ time_steps = linspace(0,tf,nf);
 
 [uatu,vatv,p] = applyboundaries2D(uatu,vatv,p); 
 %uatu(end,:) = 1.0;
-%%
-u = interp2(iatu, jatu, uatu, Xp, Yp); %scaterring interpolant
-v = interp2(iatv, jatv, vatv, Xp, Yp);
 
-u(end,:) = u(end-1,:); %Needs one more
-u(1,:)   = u(2,:);
-v(:,end) = v(:,end-1);
-v(:,1)   = v(:,2);
+Fuatu = griddedInterpolant(iatu, jatu, uatu); 
+Fvatv = griddedInterpolant(iatv, jatv, vatv); 
+u = Fuatu(Xp,Yp);
+v = Fvatv(Xp,Yp);
 
-uatv = interp2(Xp,Yp, u, iatv, jatv);
-vatu = interp2(Xp,Yp, v, iatu, jatu);
+Fu = griddedInterpolant(Xp,Yp, u);
+Fv = griddedInterpolant(Xp,Yp, v);
+
+uatv = Fu(iatv,jatv);
+vatu = Fv(iatu,jatu);
 
     figure(2);
     u_v = squeeze(uatv(:,:));
@@ -258,16 +310,16 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
     title('v')
     colorbar;
     shading interp;
-    subplot(2,2,4)
-    pcolor(Xp,Yp,cav)
-    hold on
-    quiver(Xp,Yp,u,v)
-    hold off
-    xlim([0 1])
-    ylim([0 1])
-    colorbar;
-    shading interp;
-    title(['vorticity Dt = ' num2str((time+1)*dt)])
+%     subplot(2,2,4)
+%     pcolor(Xp,Yp,cav)
+%     hold on
+%     quiver(Xp,Yp,u,v)
+%     hold off
+%     xlim([0 1])
+%     ylim([0 1])
+%     colorbar;
+%     shading interp;
+%     title(['vorticity Dt = ' num2str((time+1)*dt)])
 
 %Dy = Xp(1,3)-Xp(1,2);
 %Dx = Dy;
@@ -278,7 +330,7 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
 
 
 %% Boussinesq Pred-Corr formulation:
-%for time = 1:size(time_steps,2)
+for time = 1:size(time_steps,2)
 
     %Predict u*
 
@@ -301,31 +353,10 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
             udu_dx  = uatu(i,j)*du_dx;
             vdu_dy  = vatu(i,j)*du_dy;  
 
-            
-%             du_dx = (uatu(i,j+1)-uatu(i,j-1))/(2*Dx);
-%             du_dy = (vatu(i+1,j)-vatu(i-1,j))/(2*Dy);
-%             udu_dx  = uatu(i,j)*du_dx;
-%             vdu_dy  = uatu(i,j)*du_dy;  
-%             
-            %udu_dx = kawamura2D(u,u,Dx,i,j,'x');
-            %udv_dy = kawamura2D(u,v,Dy,i,j,'y');
-            
-            %RHS(i,j) = -udu_dx-vdu_dy + nu*(d2u_dy2+d2u_dx2);
-            
+                      
             u_star(i,j) = uatu(i,j) - dt*(udu_dx+vdu_dy - IRe*(d2u_dy2+d2u_dx2));
         end
     end
-    
-    %u_star(:,end) = u_star(:,end-1);
-    %u_star(end-1:end,end) = u_star(end-2:end-1,end-1);
-    
-    
-    
-    %RHS = D*[ Uu.*Uu - txx ; Uv.*Vv - txy_v ];
-%     RHS = D*[ Uu - txx ; Uv - txy_v ];
-%     RHS = reshape(RHS,[IMax+1 JMax+1])';
-%     RHS = applyRHSboundaries2D(RHS,IMax,JMax);
-%     u_star = SSPRK102D(u,RHS,dt);
     
     %Predict v*
      
@@ -339,31 +370,15 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
             dv_dy = (vatv(i+1,j)-vatv(i-1,j))/(2*Dy);       
             udv_dx  = uatv(i,j)*dv_dx;
             vdv_dy  = vatv(i,j)*dv_dy;
-                    
-%             dv_dx = (uatv(i,j+1)-uatv(i,j-1))/(2*Dx);
-%             dv_dy = (vatv(i+1,j)-vatv(i-1,j))/(2*Dy);       
-%             udv_dx  = vatv(i,j)*dv_dx;
-%             vdv_dy  = vatv(i,j)*dv_dy;
-                        
-            %udv_dx = kawamura2D(u,v,Dx,i,j,'x');
-            %vdv_dy = kawamura2D(v,v,Dy,i,j,'y');
-           
-            %RHS(i,j) = -udv_dx-vdv_dy + nu*(d2v_dy2+d2v_dx2);
-           
-            v_star(i,j) = vatv(i,j) - dt*(udv_dx+vdv_dy - IRe*(d2v_dy2+d2v_dx2)) -buoy(i,j);
+                              
+            v_star(i,j) = vatv(i,j) - dt*( udv_dx+vdv_dy - IRe*(d2v_dy2+d2v_dx2));% - buoy(i,j) );
            
         end
     end
 
-
-%     RHS = D*[ Uu.*Vu - txy_u  ; Vv.*Vv - tyy ];
-%     RHS = D*[ Vu - txy_u  ; Vv - tyy ];
-%     RHS = reshape(RHS,[IMax+1 JMax+1])';
-%     RHS = applyRHSboundaries2D(RHS,IMax,JMax);
-%     v_star = SSPRK102D(v,RHS,dt);
+    v_star = v_star + dt*buoy;
     
     [u_star,v_star,~] = applyboundaries2Dstar(u_star,v_star,p);
-    %u_star(end,:) = 1.0 ;
 
     figure(4);
     u_v = squeeze(u_star(:,:));
@@ -384,8 +399,13 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
     title('p')
     colorbar;
     shading interp;
+    subplot(2,2,4)
+    u_3 = squeeze(T(:,:));
+    pcolor(u_3);
+    title('T')
+    colorbar;
+    shading interp;
     
-%%
     Uu = reshape(u_star',[], 1);
     Vv = reshape(v_star',[], 1);
    
@@ -406,22 +426,24 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
     uatu = reshape(Uu,[IMax JMax-1])' ; 
     vatv = reshape(Vv,[IMax-1 JMax])' ; 
     
-    [uatu,vatv,~] = applyboundaries2D(uatu,vatv,p);
-    %uatu(end,:) = 1.0;
+    [uatu,vatv,p] = applyboundaries2D(uatu,vatv,p);   
     
-    u = interp2(iatu, jatu, uatu, Xp, Yp); %scattered interpolant
-    v = interp2(iatv, jatv, vatv, Xp, Yp);
-    p = reshape(Pp,[IMax+1 JMax+1])';
-        
-    u(end,:) = u(end-1,:);
-    u(1,:)   = u(2,:);
-    v(:,end) = v(:,end-1);
-    v(:,1)   = v(:,2);
+    Fuatu = griddedInterpolant(iatu, jatu, uatu); 
+    Fvatv = griddedInterpolant(iatv, jatv, vatv); 
+    u = Fuatu(Xp,Yp);
+    v = Fvatv(Xp,Yp);
 
-    uatv = interp2(Xp,Yp, u, iatv, jatv);
-    vatu = interp2(Xp,Yp, v, iatu, jatu);
+    p = reshape(Pp,[IMax+1 JMax+1])';
+    
+    Fu = griddedInterpolant(Xp,Yp, u);
+    Fv = griddedInterpolant(Xp,Yp, v);
+
+    uatv = Fu(iatv,jatv);
+    vatu = Fv(iatu,jatu);    
      
-     
+    uatc = Fu(X,Y);
+    vatc = Fv(X,Y); 
+    
     [~,cav] = curl(Xp,Yp,u,v);
               
     %Interpolate and refresh arrays:
@@ -438,68 +460,87 @@ vatu = interp2(Xp,Yp, v, iatu, jatu);
     for j = 2:JMax-2
       for i = 2:IMax-2
     
-            d2T_dy2 = (Tatc(i-1,j)-2*Tatc(i,j)+Tatc(i+1,j))/(Dy^2);
-            d2T_dx2 = (Tatc(i,j-1)-2*Tatc(i,j)+Tatc(i,j+1))/(Dx^2);
+            d2T_dy2 = (T(i-1,j)-2*T(i,j)+T(i+1,j))/(Dy^2);
+            d2T_dx2 = (T(i,j-1)-2*T(i,j)+T(i,j+1))/(Dx^2);
                         
-            dT_dx = (Tatc(i,j+1)-Tatc(i,j-1))/(2*Dx);
-            dT_dy = (Tatc(i+1,j)-Tatc(i-1,j))/(2*Dy);       
-            udT_dx  = uatv(i,j)*dT_dx;
-            vdT_dy  = vatv(i,j)*dT_dy;
+            dT_dx = (T(i,j+1)-T(i,j-1))/(2*Dx);
+            dT_dy = (T(i+1,j)-T(i-1,j))/(2*Dy);       
+            udT_dx  = uatc(i,j)*dT_dx;
+            vdT_dy  = vatc(i,j)*dT_dy;
                     
            
-            Tatc(i,j) = Tatc(i,j) - dt*(udT_dx+vdT_dy - IRe*(d2T_dy2+d2T_dx2));
+            T(i,j) = T(i,j) - dt*(udT_dx+vdT_dy - IRe*(d2T_dy2+d2T_dx2));
            
         end
     end
     
-    Tatc(end,:) = Tatc(end-1,:);
-    Tatc(1,:)   = Tatc(2,:);
-    Tatc(:,end) = Tatc(:,end-1);
-    Tatc(:,1)   = Tatc(:,2);
+%     T(1,:) = 0.0 ; %T(2,:);
+%     T(end,:) = 0.0 ; % T(end-1,:);    
+% 
+%     
+%     T(:,1) = 0.0 ; % T(:,2);
+%     T(:,end) = 0.0 ; %T(:,end-1);  
+%     
+     
+    dens = rho_ref + drho_dT*(T*T0-T_ref);  %EOS  
     
-    dens = rho_ref + drho_dT*(Tatc*T0-T_ref);  %EOS  
-    
-    buoy(1,:) = gforce*LStar*(dens(1,:)-rho_P0)/(rho_P0*(UStar^2));
+    buoy(1,:) = gforce*LStar*(dens(1,1:n)-rho_P0)/(rho_P0*(UStar^2));
 
-    for j=2:JMax
+    for j=1:JMax-1
         for i=1:IMax-1
 
-        buoy(i,j) = gforce*LStar*(0.5*(dens(i,j)+dens(i+1,j))-rho_P0)/(rho_P0*(UStar^2));
+            buoy(i,j) = gforce*LStar*(0.5*(dens(i,j)+dens(i+1,j))-rho_P0)/(rho_P0*(UStar^2));
 
         end
     end
+    
+    buoy(end,:) = buoy(end-1,:);
     
     figure(2);
     u_v = squeeze(u(:,:));
-    subplot(2,2,1)
+    subplot(3,2,1)
     pcolor(u_v);
     title('u')
     colorbar;
     shading interp;
-    subplot(2,2,2)
-    u_2 = squeeze(p(:,:));
-    pcolor(u_2);
-    title('p')
-    colorbar;
-    shading interp;
-    subplot(2,2,3)
+    subplot(3,2,2)
     u_3 = squeeze(v(:,:));
     pcolor(u_3);
     title('v')
     colorbar;
     shading interp;
-    subplot(2,2,4)
-    pcolor(Xp,Yp,cav)
-    hold on
-    quiver(Xp,Yp,u,v)
-    hold off
-    xlim([0 1])
-    ylim([0 1])
+    subplot(3,2,3)
+    u_2 = squeeze(p(:,:));
+    pcolor(u_2);
+    title('p')
     colorbar;
     shading interp;
-    title(['vorticity Dt = ' num2str((time+1)*dt)])
+    subplot(3,2,4)
+    u_3 = squeeze(T(:,:));
+    pcolor(u_3);
+    title(['T ' num2str((time+1)*dt)])
+    colorbar;
+    shading interp;
+    subplot(3,2,5)
+    u_3 = squeeze(buoy(:,:));
+    pcolor(u_3);
+    title(['buoy'])
+    colorbar;
+    shading interp;
+    
+    
+    
+%     pcolor(Xp,Yp,cav)
+%     hold on
+%     quiver(Xp,Yp,u,v)
+%     hold off
+%     xlim([0 1])
+%     ylim([0 1])
+%     colorbar;
+%     shading interp;
+%     title(['vorticity Dt = ' num2str((time+1)*dt)])
 
 
-%end
+end
 
 
